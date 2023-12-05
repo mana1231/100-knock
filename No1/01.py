501ff6a56727bb42f7cd27c177f3b75f4deba2c8
manabekota <152841171+mana1231@users.noreply.github.com>
2023-12-05 15:37:14 +0900

    Update 01.py

--- No1/01.py
+++ No1/01.py
@@ -1,9 +1,3 @@
-def take_odd_words(text):
-    result = ""
-    for i in range(0,8,2):
-        result += text[i]
-    return result
-
 main_text = "パタトクカシーー"
-result =  take_odd_words(main_text)
+result = main_text[::2]
 print(result)
