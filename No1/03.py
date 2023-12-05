f791d2d4c4dc3bf850fda526dcd492892b3fc471
manabekota <152841171+mana1231@users.noreply.github.com>
2023-12-05 15:37:52 +0900

    Update 03.py

--- No1/03.py
+++ No1/03.py
@@ -1,13 +1,4 @@
-def remove_punctuation(text):
-    text = ''.join(tmp if tmp.isalpha() or tmp.isspace() else ' ' for tmp in text)
-    return text
-
-def words_len(text):
-    text = remove_punctuation(text)
-    word_len = [len(word) for word in text.split()]
-    return word_len
-
-
 main_text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-result = words_len(main_text)
+text = ''.join(tmp if tmp.isalpha() or tmp.isspace() else ' ' for tmp in main_text)
+result = [len(word) for word in text.split()]
 print(result)
