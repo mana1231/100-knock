58dc4c32237c1e64a2dce75c5f701e77a7f492ca
manabekota <152841171+mana1231@users.noreply.github.com>
2024-01-23 15:24:21 +0900

    Create 23.py

---
+++ No3/23.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import re
+
+def load_json_for_uk():
+    data = pd.read_json('jawiki-country.json', lines=True, encoding='utf-8')
+    uk_article = data.query('title=="イギリス"')['text'].values[0]
+
+    return uk_article
+
+def extract_sections(text):
+    section_pattern = re.compile(r'(={2,})([^=]+)\1') # (==+) -> ={2,}
+    matches = section_pattern.findall(text)
+
+    sections = [(match[1].strip(), len(match[0]) - 1) for match in matches]
+
+    return sections
+
+text_uk = load_json_for_uk()
+sections = extract_sections(text_uk)
+
+for section, level in sections:
+    print(f"name:{section}    level:{level}")
